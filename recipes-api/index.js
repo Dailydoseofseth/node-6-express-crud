404eb68b33141f3dcb4ca057b6ef87038337b480
dailydoseofseth <dailydoseofseth@gmail.com>
2026-07-02 14:48:11 -0600

    initial commit

--- recipes-api/index.js
+++ recipes-api/index.js
@@ -1,27 +1,109 @@
 // ---------------------------------
 // Boilerplate Code to Set Up Server
 // ---------------------------------
+import express from "express";
+import fs from "fs/promises";
+// import cors from "cors";
+// import { getAllRecipes, getOneRecipe, getAllRecipeNames, getRecipesCount } from "./recipes.js";
 
+const app = express();
+
+const port = 3000;
+
+// app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is running HOT on http://localhost:${port}`);
+});
 // ---------------------------------
 // Helper Functions
 // ---------------------------------
 
 // 1. getAllRecipes()
-
+async function getAllRecipes() {
+  // READ IT
+  const data = await fs.readFile("recipes-data.json", "utf-8");
+  // PARSE IT
+  const parsedData = JSON.parse(data);
+  // LOG IT
+  console.log("DATA SAY WHAT RECIPES??:", parsedData);
+  // BOP IT (...errr... we meant RETURN IT!!!)
+  return parsedData;
+}
 // 2. getOneRecipe(index)
+async function getOneRecipe(index) {
+  // READ IT
+  const data = await fs.readFile("recipes-data.json", "utf-8");
+  // PARSE IT
+  const parsedData = JSON.parse(data);
+  // STORE IT & CAPTURE IT
+  const recipe = parsedData[index];
+  // BOP IT!!! .....ERRR... DROP IT!!! .....ERRRRR we meant RETURN IT!!!
+  return recipe;
+}
 
 // 3. getAllRecipeNames()
+async function getAllRecipeNames() {
+  // READ IT
+  const data = await fs.readFile("recipes-data.json", "utf-8");
+  // PARSE IT
+  const parsedData = JSON.parse(data);
+  // STORE IT & MAP IT
+  const recipeNames = parsedData.map((recipe) => recipe.name);
+  // RETURN IT
+  return recipeNames;
+}
 
 // 4. getRecipesCount()
+async function getRecipesCount() {
+  // READ IT
+  const data = await fs.readFile("recipes-data.json", "utf-8");
+  // PARSE IT
+  const parsedData = JSON.parse(data);
+  // STORE IT & COUNT IT
+  const recipesCount = parsedData.length;
+  // RETURN IT
+  return recipesCount;
+}
 
 // ---------------------------------
 // API Endpoints
 // ---------------------------------
 
 // 1. GET /get-all-recipes
+app.get("/get-all-recipes", async (req, res) => {
+  // call the helper FUNC. Store in VAR.
+  const recipes = await getAllRecipes();
+  // return the VAR to the endpoint
+  res.json(recipes);
+});
 
 // 2. GET /get-one-recipe/:index
+app.get("/get-one-recipe/:index", async (req, res) => {
+  // capture the index from the URL
+  const index = req.params.index;
+  // call()... the HELPER FUNC .THEN, Store in VAR (recipe)
+  const recipe = await getOneRecipe(index);
+  // return the VAR to the endpoint
+  res.json(recipe);
+  console.log("RECIPE SAYS:", recipe);
+});
 
 // 3. GET /get-all-recipe-names
+app.get("/get-all-recipe-names", async (req, res) => {
+  // call the helper FUNC. Store in VAR.
+  const recipeNames = await getAllRecipeNames();
+  // return the VAR to the endpoint
+  res.json(recipeNames);
+  console.log("RECIPE NAMES SAYS:", recipeNames);
+});
 
 // 4. GET /get-recipes-count
+app.get("/get-recipes-count", async (req, res) => {
+  // call the helper FUNC. Store in VAR.
+  const recipesCount = await getRecipesCount();
+  // return the VAR to the endpoint
+  res.json({ count: recipesCount });
+  console.log("RECIPES COUNT SAYS:", recipesCount);
+});
